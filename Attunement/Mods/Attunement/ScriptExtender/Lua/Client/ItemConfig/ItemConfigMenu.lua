4e84f2f88b81d41cd9f34d2de7bb74b067cf3879
osirisofgit <github.com.j95fw@passmail.net>
2024-12-23 14:32:38 -0600

    update stuff

---
+++ Attunement/Mods/Attunement/ScriptExtender/Lua/Client/ItemConfig/ItemConfigMenu.lua
@@ -0,0 +1,6 @@
+Mods.BG3MCM.IMGUIAPI:InsertModMenuTab(ModuleUUID, "Item Configuration",
+	--- @param tabHeader ExtuiTreeParent
+	function(tabHeader)
+		tabHeader.TextWrapPos = 0
+
+	end)
---
+++ Attunement/Mods/Attunement/ScriptExtender/Lua/Client/Rules/RulesMenu.lua
@@ -0,0 +1,6 @@
+Mods.BG3MCM.IMGUIAPI:InsertModMenuTab(ModuleUUID, "Rules",
+	--- @param tabHeader ExtuiTreeParent
+	function(tabHeader)
+		tabHeader.TextWrapPos = 0
+
+	end)
