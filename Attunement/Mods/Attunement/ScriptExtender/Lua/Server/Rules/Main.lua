3f07f8786357b8bafbac6d8e8884a4149c13e430
osirisofgit <github.com.j95fw@passmail.net>
2024-12-28 15:54:57 -0600

    feexes

--- Attunement/Mods/Attunement/ScriptExtender/Lua/Server/Rules/Main.lua
+++ Attunement/Mods/Attunement/ScriptExtender/Lua/Server/Rules/Main.lua
@@ -67,8 +67,6 @@ local playerSubs = {}
 Ext.Osiris.RegisterListener("LevelGameplayReady", 2, "after", function(levelName, isEditorMode)
 	local functionsToRun = BuildRelevantStatFunctions()
 	if #functionsToRun > 0 then
-		Osi.ShowNotification(Osi.GetHostCharacter(), "Beginning Attunement Initialization - game may lag for a moment if you have a lot of items")
-
 		for _, template in pairs(Ext.Template.GetAllRootTemplates()) do
 			if template.TemplateType == "item" then
 				---@type ItemStat
@@ -84,7 +82,13 @@ Ext.Osiris.RegisterListener("LevelGameplayReady", 2, "after", function(levelName
 				end)
 
 				if not success then
-					Logger:BasicWarning("Error processing stat %s for template %s: %s", stat.Name, template.Name, err)
+					local mod = Ext.Mod.GetMod(stat.ModId).Info
+					Logger:BasicWarning("Error processing stat %s (from Mod '%s' by '%s') for template %s: %s",
+						stat.Name,
+						mod.Name,
+						mod.Author,
+						template.Name,
+						err)
 				end
 			end
 		end
@@ -132,8 +136,9 @@ Ext.Osiris.RegisterListener("LevelGameplayReady", 2, "after", function(levelName
 							for cost in string.gmatch(stat.UseCosts, "([^;]+)") do
 								local costName = string.match(cost, "^[^:]+")
 
-								local resource
 								if string.match(costName, "^.*Attunement$") then
+									local resourceToModify
+
 									local cachedResourceID = cachedResources[costName]
 									if not cachedResourceID then
 										for _, actionResourceId in pairs(Ext.StaticData.GetAll("ActionResource")) do
@@ -146,16 +151,16 @@ Ext.Osiris.RegisterListener("LevelGameplayReady", 2, "after", function(levelName
 											end
 										end
 									end
-									resource = resources[cachedResourceID][1]
-									if resource.Amount == 0 then
+									resourceToModify = resources[cachedResourceID][1]
+									if resourceToModify.Amount == 0 then
 										Osi.Unequip(player, equippedItem)
 										if not sentNotification then
 											sentNotification = true
 											Osi.ShowNotification(player, playerEntity.CustomName.Name .. " had items unequipped due to exceeding Attunement/Rarity Equip Limits")
 										end
 									else
-										resource.Amount = resource.Amount - 1
-										resource.MaxAmount = resource.Amount
+										resourceToModify.Amount = resourceToModify.Amount - 1
+										resourceToModify.MaxAmount = resourceToModify.Amount
 									end
 								end
 							end
@@ -173,7 +178,6 @@ Ext.Osiris.RegisterListener("LevelGameplayReady", 2, "after", function(levelName
 				playerSubs[player] = nil
 
 				if not next(playerSubs) then
-					Osi.ShowNotification(Osi.GetHostCharacter(), "Attunement Initialization Complete")
 					Logger:BasicInfo("Initialization complete")
 				end
 			end)
@@ -189,17 +193,12 @@ Ext.Osiris.RegisterListener("Equipped", 2, "after", function(item, character)
 end)
 
 Ext.Osiris.RegisterListener("AddedTo", 3, "after", function(item, inventoryHolder, addType)
-	if MCM.Get("enabled") then
-		---@type ItemTemplate
-		local template = Ext.Template.GetTemplate(string.sub(Osi.GetTemplate(item), -36))
+	if MCM.Get("enabled") and Osi.IsEquipable(item) == 1 and Osi.IsEquipped(item) == 0 then
+		---@type ItemStat
+		local stat = Ext.Stats.Get(Osi.GetStatString(item))
 
-		if template.TemplateType == "item" and Osi.IsEquipped(item) == 0 then
-			---@type ItemStat
-			local stat = Ext.Stats.Get(template.Stats)
-
-			if string.find(stat.UseCosts, "Attunement") then
-				Osi.ApplyStatus(item, "ATTUNEMENT_REQUIRES_ATTUNEMENT_STATUS", -1, 1)
-			end
+		if stat and string.find(stat.UseCosts, "Attunement") then
+			Osi.ApplyStatus(item, "ATTUNEMENT_REQUIRES_ATTUNEMENT_STATUS", -1, 1)
 		end
 	end
 end)
@@ -211,11 +210,12 @@ Ext.Osiris.RegisterListener("Unequipped", 2, "after", function(item, character)
 		local charEntity = Ext.Entity.Get(character)
 		local resources = charEntity.ActionResources.Resources
 
-		---@type ItemTemplate
-		local template = Ext.ServerTemplate.GetTemplate(string.sub(Osi.GetTemplate(item), -36))
-
 		---@type ItemStat
-		local stat = Ext.Stats.Get(template.Stats)
+		local stat = Ext.Stats.Get(Osi.GetStatString(item))
+
+		if not stat then
+			return
+		end
 
 		local foundAttunement = false
 
